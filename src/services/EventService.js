c1b40f9e194242e86604c8a784629370279037b5
fardisaz <amoozadeh73@yahoo.com>
2021-03-02 11:40:51 +0100

    api-call-with-axios

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+//this is a single axios instance , we can use for our entire app
+const apiClient = axios.create({
+  //this is the base URL for all calls to use
+  baseURL: 'https://my-json-server.typicode.com/fardisaz/real-world-vue',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+export default {
+  getEvents() {
+    //this '/events' will be added to the baseURL
+    return apiClient.get('/events')
+  }
+}
